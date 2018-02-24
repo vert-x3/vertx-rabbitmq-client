94ec32ca40603340698593c1a5ef1f37475026f3
Sammers21 <titantins@gmail.com>
2018-02-24 21:55:18 +0300

    add more comment to RabbitMQueue

--- src/main/java/io/vertx/rabbitmq/RabbitMQueue.java
+++ src/main/java/io/vertx/rabbitmq/RabbitMQueue.java
@@ -7,23 +7,52 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.streams.ReadStream;
 
 /**
- * A stream of messages from a rabbitmq queue
+ * A stream of messages from a rabbitmq queue.
  */
 @VertxGen
 public interface RabbitMQueue extends ReadStream<JsonObject> {
 
+  /**
+   * Set an exception handler on the read stream.
+   *
+   * @param exceptionHandler the exception handler
+   * @return a reference to this, so the API can be used fluently
+   */
   @Override
   RabbitMQueue exceptionHandler(Handler<Throwable> exceptionHandler);
 
+  /**
+   * Set a message handler. As message appear in a queue, the handler will be called with the message.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
   @Override
   RabbitMQueue handler(Handler<JsonObject> messageArrived);
 
+  /**
+   * Pause the stream of incoming messages from queue.
+   * <p>
+   * The messages will continue to arrive, but they will be stored in a internal queue.
+   * If the queue size would exceed the limit provided by {@link RabbitMQueue#size(int)}, then incoming messages will be discarded.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
   @Override
   RabbitMQueue pause();
 
+  /**
+   * Resume reading from a queue. Flushes internal queue.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
   @Override
   RabbitMQueue resume();
 
+  /**
+   * Set an end handler. Once the stream has canceled successfully, the handler will be called.
+   *
+   * @return a reference to this, so the API can be used fluently
+   */
   @Override
   RabbitMQueue endHandler(Handler<Void> endHandler);
 
@@ -38,7 +67,6 @@ public interface RabbitMQueue extends ReadStream<JsonObject> {
    * @return a consumer tag
    */
   String consumerTag();
-
 
   /**
    * Stop message consumption from a queue.
