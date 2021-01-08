6877b07824995bcc617ed777918c223257de5318
YaytayAtWork <jim.talbut@groupgti.com>
2021-01-08 08:53:39 +0000

    Disable autoRecovery for RabbitMQClientTransientQueueTest

--- src/test/java/io/vertx/rabbitmq/RabbitMQClientTransientQueueTest.java
+++ src/test/java/io/vertx/rabbitmq/RabbitMQClientTransientQueueTest.java
@@ -16,7 +16,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.Test;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
