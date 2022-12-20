adef85a00b1b2157879e4a4cf733750b58101a19
Yaytay <jim-gitkraken@spudsoft.co.uk>
2022-12-20 11:58:12 +0000

    Correct SSL configuration following SSLHelper changes

--- src/main/java/io/vertx/rabbitmq/impl/RabbitMQClientImpl.java
+++ src/main/java/io/vertx/rabbitmq/impl/RabbitMQClientImpl.java
@@ -113,7 +113,7 @@ public class RabbitMQClientImpl implements RabbitMQClient, ShutdownListener {
     cf.setRequestedChannelMax(config.getRequestedChannelMax());
     cf.setNetworkRecoveryInterval(config.getNetworkRecoveryInterval());
     cf.setAutomaticRecoveryEnabled(config.isAutomaticRecoveryEnabled());
-
+    
     if (config.isSsl()) {
       //The RabbitMQ Client connection needs a JDK SSLContext, so force this setting.
       config.setSslEngineOptions(new JdkSSLEngineOptions());
@@ -129,6 +129,7 @@ public class RabbitMQClientImpl implements RabbitMQClient, ShutdownListener {
       JdkSslContext ctx = (JdkSslContext) provider.createContext((VertxInternal) vertx);
       cf.useSslProtocol(ctx.context());
     }
+    
 
     if (config.isNioEnabled()) {
       cf.useNio();
