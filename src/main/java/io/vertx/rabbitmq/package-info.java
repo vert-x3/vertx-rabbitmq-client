aca81db20a846830744c9a74425b4f516f606e9d
Julien Viet <julien@julienviet.com>
2024-12-10 16:47:23 +0100

    Remove usage of useFutures member

--- src/main/java/io/vertx/rabbitmq/package-info.java
+++ src/main/java/io/vertx/rabbitmq/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(name = "vertx-rabbitmq", groupPackage = "io.vertx", useFutures = true)
+@ModuleGen(name = "vertx-rabbitmq", groupPackage = "io.vertx")
 package io.vertx.rabbitmq;
 
 import io.vertx.codegen.annotations.ModuleGen;
